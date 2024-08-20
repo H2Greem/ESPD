9ef7e37ff28ec0e79fad7ad8939f3ea0a9d5fd35
dragos-eu <Dragos-Constantin.STOICA@ext.ec.europa.eu>
2024-08-20 15:04:50 +0200

    UUID in place for SC and EG

--- excel2espd.js
+++ excel2espd.js
@@ -1253,7 +1253,7 @@ function render_request(obj, part = espd_request, EG_FLAG = true) {
             const element = obj[elm]
             switch (element.type) {
                 case "CRITERION":
-                    let c_id = (element.requestpath.indexOf('OT') != -1)?element.requestpath:element.elementUUID
+                    let c_id = (element.requestpath.indexOf('_OT_') != -1)?element.requestpath:element.elementUUID
                     tmp = part.com(` Criterion: ${element.name} `)
                         .ele('@cac', 'TenderingCriterion')
                         .ele('@cbc', 'ID', { 'schemeID': 'Criterion', 'schemeAgencyID': 'OP', 'schemeVersionID': schemeVersionID }).txt(c_id).up()
